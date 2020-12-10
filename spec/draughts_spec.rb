9e34df883d34719a2f43918bde30871d4dbecffc
Rhys Morris <rhysmorris08@gmail.com>
2020-12-10 16:51:19 +1100

    Added rspec as dependency

---
+++ spec/draughts_spec.rb
@@ -0,0 +1,30 @@
+require_relative "../gameboard.rb"
+require_relative "../marker.rb"
+require_relative "../red_marker.rb"
+require_relative "../black_marker.rb"
+require_relative "../king_marker.rb"
+
+# Tests for Marker class
+describe 'Marker' do
+    
+end
+
+# Tests for RedMarker class
+describe 'RedMarker' do
+    
+end
+
+# Tests for BlackMarker class
+describe 'BlackMarker' do
+    
+end
+
+#Tests for KingMarker classes
+describe 'KingMarker' do
+    
+end
+
+#Tests for Gameboard class
+describe 'Gameboard' do
+    
+end
